fac219388d22403c5b0e69fde2440a7c42b7c446
leo-fp <2491587009@qq.com>
2019-10-19 18:19:47 +0800

    restructure

--- main.py
+++ main.py
@@ -166,31 +166,5 @@ class TransOFL(Translator_ab):
 
 
 if __name__ == "__main__":
-    #print "hello world" 
-    conf = parse.parse_conf()
-    appid = conf[0]
-    secretKey = conf[1] 
-    #print appid
-    #print secretKey
-     
-    httpClient = None
-    myurl = '/api/trans/vip/translate'
-    q = sys.argv[1]
-    #print q
-    fromLang = parse.parse_from(sys.argv[1])
-    toLang = 'zh'
-    #如果源语种是中文，则换英文输出
-    if(fromLang == toLang):
-        toLang = 'en'
-    salt = random.randint(32768, 65536)
-
-    sign = appid+q+str(salt)+secretKey
-    m1 = md5.new()
-    m1.update(sign)
-    sign = m1.hexdigest()
-    myurl = myurl+'?appid='+appid+'&q='+urllib.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
-     
-    #============test===============
     transOL = Creator.createTranslator(TransOL)
-    #transOFL = Creator.createTranslator(TransOFL)
     transOL.launch(sys.argv[1])
